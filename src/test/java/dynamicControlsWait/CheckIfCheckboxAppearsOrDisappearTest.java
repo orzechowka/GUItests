05061ee46071a98801f6d6f8547962ccc5117e38
a.warzecha <a.warzecha89@gmail.com>
2018-08-26 14:28:56 +0200

    Check if checkbox appears or disappears

---
+++ src/test/java/dynamicControlsWait/CheckIfCheckboxAppearsOrDisappearTest.java
@@ -0,0 +1,53 @@
+package dynamicControlsWait;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+public class CheckIfCheckboxAppearsOrDisappearTest {
+
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new FirefoxDriver();
+        driver.get("http://the-internet.herokuapp.com/");
+        driver.findElement(By.linkText("Dynamic Controls")).click();
+    }
+
+    @Test
+    public void checkIfCheckboxDisappear() {
+        WebElement btn = driver.findElement(By.id("btn"));
+        btn.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        Boolean exist = wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("checkbox")));
+        assertTrue(exist);
+        assertEquals("It's gone!",getMessage());
+
+        btn.click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("checkbox")));
+
+        assertEquals("It's back!", getMessage());
+    }
+
+    private String getMessage() {
+        WebElement textMessage = driver.findElement(By.id("message"));
+        return textMessage.getText();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
